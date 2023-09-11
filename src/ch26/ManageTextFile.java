5d214d0dc8c9e80d8df5e75099fe300660970bf9
k9want <zndwlrnjr26@daum.net>
2023-09-11 23:44:09 +0900

    :technologist: Add ch26 - ManageTextFile class method

--- src/ch26/ManageTextFile.java
+++ src/ch26/ManageTextFile.java
@@ -1,6 +1,7 @@
 package ch26;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class ManageTextFile {
     public static void main(String[] args) {
@@ -9,6 +10,8 @@ public class ManageTextFile {
         String fullPath = "C:"+ File.separator + "Users" + File.separator + "USER" + File.separator + "Desktop" + File.separator+"text" + File.separator+ "numbers.txt";
         System.out.println(fullPath);
         manager.writeFile(fullPath, numberCnt);
+        manager.readFile(fullPath);
+        manager.readFileWithScanner(fullPath);
     }
 
     private void writeFile(String fileName, int numberCnt) {
@@ -44,5 +47,60 @@ public class ManageTextFile {
                 }
             }
         }
+    }
+
+    private void readFile(String fileName) {
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileReader = new FileReader(fileName);
+            bufferedReader = new BufferedReader(fileReader);
+            String data;
+            while ((data = bufferedReader.readLine()) != null) {
+                System.out.println(data);
+            }
+            System.out.println("readFile finished !!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if ( fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void readFileWithScanner(String fileName) {
+        File file = new File(fileName);
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+            System.out.println("Read Success !!!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+
     }
 }
